ae3a04a25f7b65db2bd50bfa6af952a3d206c88f
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2020-01-05 20:10:40 +0530

    Made a PWA

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "bookmark-app-cache"
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(
+                [
+                    '/',
+                    'index.html',
+                    'style.css',
+                    'script.js'
+                ]
+            )
+        })
+    )
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request);
+        })
+    );
+});
