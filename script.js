bf6d76ef13ad391af88171e45bd8e405a0be054b
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2020-05-07 22:02:50 +0530

    Some fixes for serviceworker

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 //     }
 // ]
 
-let sites = JSON.parse(localStorage.getItem('sites')) || [];
+let sites = JSON.parse(localStorage.getItem("sites")) || [];
 
 let siteList = document.querySelector("#siteList");
 
@@ -18,23 +18,23 @@ let form = document.querySelector("form");
 let siteName = document.querySelector("#site_name");
 let siteLink = document.querySelector("#site_url");
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    let siteObject = {
-        id: sites.length,
-        site_name: siteName.value,
-        site_link: siteLink.value
-    }
-    sites = [siteObject, ...sites]
-    siteName.value = ""
-    siteLink.value = ""
-    render();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let siteObject = {
+    id: sites.length,
+    site_name: siteName.value,
+    site_link: siteLink.value,
+  };
+  sites = [siteObject, ...sites];
+  siteName.value = "";
+  siteLink.value = "";
+  render();
+});
 
 const render = () => {
-    siteList.innerHTML = "";
-    sites.forEach(site => {
-        siteList.innerHTML += `
+  siteList.innerHTML = "";
+  sites.forEach((site) => {
+    siteList.innerHTML += `
         <div class="site">
             <h3>${site.site_name}</h3>
             <div>
@@ -42,14 +42,45 @@ const render = () => {
                 <div class="del btn" onclick="del(${site.id})">Delete</>
             </div>
         </div>
-        `
-    });
-    localStorage.setItem("sites", JSON.stringify(sites))
-}
+        `;
+  });
+  localStorage.setItem("sites", JSON.stringify(sites));
+};
 
 render();
 
 let del = (id) => {
-    sites = sites.filter(site => site.id !== id)
-    render();
+  sites = sites.filter((site) => site.id !== id);
+  render();
+};
+
+//sw initialization
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("sw.js")
+    .then((sw) => console.log("Service work registration successful"))
+    .catch((err) => console.log("Error"));
+} else {
+  console.log("Service Worker not supported!");
 }
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  window.deferredPrompt = e;
+  installButton.style.display = "block";
+});
+
+const installButton = document.getElementById("install_button");
+
+installButton.addEventListener("click", () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  promptEvent.userChoice.then((result) => {
+    console.log("userChoice", result);
+    window.deferredPrompt = null;
+    installButton.style.display = "none";
+  });
+});
--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const CACHE_NAME = "bookmark-app-cache-v2"
+const CACHE_NAME = "bookmark-app-cache-v2";
 
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(
-                [
-                    './index.html',
-                    './style.css',
-                    './script.js',
-                ]
-            )
-        })
-    )
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icon.png",
+  "iconx512.png",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
